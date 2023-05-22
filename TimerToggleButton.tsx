70ef6e6ffae5725ce30dc2aeaf5a35b96fe0af84
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-05-22 15:45:19 +0300

    Update TimerToggleButton.tsx

--- TimerToggleButton.tsx
+++ TimerToggleButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button } from 'react-native'
+import { StyleSheet, Pressable, View } from 'react-native'
+import {FontAwesome} from '@expo/vector-icons'
 
 type Props = {
     isTimerRunning: boolean;
@@ -9,6 +10,26 @@ type Props = {
 
 export const TimerToggleButton: React.FC<Props> = ({ isTimerRunning, stopTimer, startTimer}) => {
   return (
-        <Button title={isTimerRunning? 'Stop Timer' :'Start Timer'} onPress={isTimerRunning? stopTimer : startTimer} />
+    <Pressable onPress={isTimerRunning? stopTimer : startTimer}>
+       <View style={styles.container}>
+            <FontAwesome name={isTimerRunning ? "pause" : "play"} size={125}  style={styles.icon} />
+        </View> 
+    </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        alignSelf: "center",
+        color: "#fff"
+    },
+    container: {
+        borderWidth: 5,
+        width: 250,
+        height: 250,
+        borderRadius: 250 / 2,
+        justifyContent: "center",
+        borderColor: "#fff",
+        marginVertical: 50,
+    },
+})
